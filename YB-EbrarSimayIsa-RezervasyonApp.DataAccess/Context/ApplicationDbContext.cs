e22fda7619148489930c0cde217220d12d611c4f
Simay Seyman <simayseyman@gmail.com>
2024-07-22 13:43:01 +0300

    Room ve RoomType Repository oluşturuldu.

--- YB-EbrarSimayIsa-RezervasyonApp.DataAccess/Context/ApplicationDbContext.cs
+++ YB-EbrarSimayIsa-RezervasyonApp.DataAccess/Context/ApplicationDbContext.cs
@@ -24,10 +24,12 @@ namespace YB_EbrarSimayIsa_RezervasyonApp.DataAccess.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Simay Conn
-            //optionsBuilder.UseSqlServer("Data Source = BPC12\\SQLEXPRESS; Initial Catalog = RezervasyonDb ; Integrated Security = True; Trust Server Certificate = True");
-            
+            optionsBuilder.UseSqlServer("Data Source = BPC12\\SQLEXPRESS; Initial Catalog = RezervasyonDb ; Integrated Security = True; Trust Server Certificate = True");
+
+            //ebrar
+            optionsBuilder.UseSqlServer("Data Source = EBRAR; Initial Catalog = RezervasyonDb ; Integrated Security = True; Trust Server Certificate = True");
             //İsa Conn
-            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = RezervasyonDb ; Integrated Security = True; Trust Server Certificate = True");
+            //optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = RezervasyonDb ; Integrated Security = True; Trust Server Certificate = True");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- YB-EbrarSimayIsa-RezervasyonApp.DataAccess/Repositories/RoomRepository.cs
+++ YB-EbrarSimayIsa-RezervasyonApp.DataAccess/Repositories/RoomRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YB_EbrarSimayIsa_RezervasyonApp.DataAccess.Context;
+using YB_EbrarSimayIsa_RezervasyonApp.Entities.Models;
 
 namespace YB_EbrarSimayIsa_RezervasyonApp.DataAccess.Repositories
 {
-    public class RoomRepository
+    public class RoomRepository : GenericRepository<Room>
     {
+        public RoomRepository(ApplicationDbContext context) : base(context)
+        {
+        }
     }
 }
