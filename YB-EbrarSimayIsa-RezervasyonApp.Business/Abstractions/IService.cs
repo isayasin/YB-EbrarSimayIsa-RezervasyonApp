492758c93ff0b8ec152ec44a676515ebbd72533a
Simay Seyman <simayseyman@gmail.com>
2024-07-22 14:30:20 +0300

    IService eklendi

---
+++ YB-EbrarSimayIsa-RezervasyonApp.Business/Abstractions/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_EbrarSimayIsa_RezervasyonApp.Entities.Abstractions;
+
+namespace YB_EbrarSimayIsa_RezervasyonApp.Business.Abstractions
+{
+    public interface IService<T> where T : Entity
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+        T? GetById(Guid id);
+        IEnumerable<T>? GetAll();
+    }
+}
