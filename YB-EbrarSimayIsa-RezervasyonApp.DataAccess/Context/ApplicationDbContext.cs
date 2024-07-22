9ed56201dbb6822610548b717437aa71e0b0357a
Simay Seyman <simayseyman@gmail.com>
2024-07-22 13:00:16 +0300

    Context class ı oluşturuldu.

---
+++ YB-EbrarSimayIsa-RezervasyonApp.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_EbrarSimayIsa_RezervasyonApp.Entities.Models;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace YB_EbrarSimayIsa_RezervasyonApp.DataAccess.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = BPC12\\SQLEXPRESS; Initial Catalog = RezervasyonDb ; Integrated Security = True; Trust Server Certificate = True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
