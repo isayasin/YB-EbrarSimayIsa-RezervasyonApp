6cdf6f57b24babfea45a76140692e7c5022af6f3
Ebrar LEVENT <ebrarleventt@gmail.com>
2024-07-22 11:51:47 +0300

    Base Class Olusturuldu.

---
+++ YB-EbrarSimayIsa-RezervasyonApp.Entities/Abstractions/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YB_EbrarSimayIsa_RezervasyonApp.Entities.Abstractions
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            ID = Guid.NewGuid();
+        }
+
+        public Guid ID { get; set; }
+        public bool ISActive { get; set; }
+        public DateTime CreateAtDate { get; set; }
+        public DateTime UpdateAtDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
