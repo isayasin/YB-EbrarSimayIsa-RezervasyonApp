0ae24d6bf2e220f6d670eac8ffdeea451a1dc160
Simay Seyman <simayseyman@gmail.com>
2024-07-22 13:36:47 +0300

    repo oluşturuldu

---
+++ YB-EbrarSimayIsa-RezervasyonApp.DataAccess/Repositories/RoomRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YB_EbrarSimayIsa_RezervasyonApp.DataAccess.Repositories
+{
+    public class RoomRepository
+    {
+    }
+}
