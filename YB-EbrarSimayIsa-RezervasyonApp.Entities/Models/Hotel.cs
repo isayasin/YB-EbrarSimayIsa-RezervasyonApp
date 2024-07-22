6259e1c1d7040748c76ab8095e5b45b4d15ddbbb
Ebrar LEVENT <ebrarleventt@gmail.com>
2024-07-22 12:10:45 +0300

    Hotel ve Staff classlari olusturuldu.

---
+++ YB-EbrarSimayIsa-RezervasyonApp.Entities/Models/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_EbrarSimayIsa_RezervasyonApp.Entities.Abstractions;
+
+namespace YB_EbrarSimayIsa_RezervasyonApp.Entities.Models
+{
+    public class Hotel : Entity
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public int Stars { get; set; }
+        public DateTime CheckinTime { get; set; }
+        public DateTime CheckoutTime { get; set; }
+        public ICollection<Staff>? Staffs { get; set; }
+
+    }
+
+}
---
+++ YB-EbrarSimayIsa-RezervasyonApp.Entities/Models/Staff.cs
@@ -0,0 +1,20 @@
+﻿using YB_EbrarSimayIsa_RezervasyonApp.Entities.Abstractions;
+
+namespace YB_EbrarSimayIsa_RezervasyonApp.Entities.Models
+{
+    public class Staff : Entity
+    {
+        public Guid HotelID { get; set; }
+        public Hotel? Hotel { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public double Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public DateTime HireDate { get; set; }
+
+    }
+
+}
