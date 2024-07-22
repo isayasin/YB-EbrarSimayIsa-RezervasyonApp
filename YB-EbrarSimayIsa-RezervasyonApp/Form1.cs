0971c7febb3ede024f620f0d34f1116fc9670108
isayasin <isayasinkuru@gmail.com>
2024-07-22 11:14:37 +0300

    Add project files.

---
+++ YB-EbrarSimayIsa-RezervasyonApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace YB_EbrarSimayIsa_RezervasyonApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
